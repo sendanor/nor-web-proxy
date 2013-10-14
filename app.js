921a869b8c6c15a52e131f2a1588780a343fc606
Jaakko-Heikki Heusala <jhh@sendanor.com>
2013-10-14 12:17:51 +0300

    Updated

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
+#!/usr/bin/env node
 /* app.js */
+
+var proxy_host = process.env.HOST || '0.0.0.0';
+var proxy_port = process.env.PORT || 80;
+var proxy_dir = process.env.VARDIR || '/var';
+var proxy_logdir = process.env.LOGDIR || proxy_dir+'/log';
+var proxy_rundir = process.env.RUNDIR || proxy_dir+'/run';
+var proxy_pid = process.env.PIDFILE || proxy_rundir + '/node-proxy.pid';
+var proxy_log = process.env.LOGFILE || proxy_logdir + '/node-proxy.log';
+var proxy_user = process.env.PROXY_USER || "www-data";
+var proxy_group = process.env.PROXY_GROUP || "www-data";
 
 var tld = require('tld');
 var http = require('http');
 var httpProxy = require('http-proxy');
 var getent = require('getent');
 
-httpProxy.createServer(function (req, res, proxy) {
-	var domain = tld.registered(req.host);
-	var group = domain.replace(/\./g, '_').toLowerCase();
-	var g = getent.group(group);
-	var port;
+function start_app() {
+	var proxy = httpProxy.createProxyServer({});
 
+	process.chdir("/");
 
-	proxy.proxyRequest(req, res, {
-		host: 'www1.sendanor.com',
-		port: port
+	var server = http.createServer(function (req, res) {
+		var domain = tld.registered(req.host);
+		var group = domain.replace(/\./g, '_').toLowerCase();
+		var g = getent.group(group);
+		var port = 7000 + g.gid;
+		proxy.web(req, res, {
+			host: 'www1.sendanor.com',
+			port: port
+		});
+	}).listen(proxy_port, proxy_host, function() {
+		if (process.getuid() === 0) {
+			process.initgroups(proxy_user, proxy_group);
+			process.setgid(proxy_group);
+			process.setuid(proxy_user);
+		}
 	});
-}).listen(8000);
+}
+
+start_app();
 
 /* EOF */
