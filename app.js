eba96141b0d57c4daceaecb07bc548c25c5effb6
Jaakko-Heikki Heusala <jhh@sendanor.com>
2013-10-14 14:52:18 +0300

    Updated

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var proxy_log = process.env.LOGFILE || proxy_logdir + '/nor-web-proxy.log';
 var proxy_user = process.env.PROXY_USER || "www-data";
 var proxy_group = process.env.PROXY_GROUP || "www-data";
 
+var util = require('util');
 var tld = require('tld');
 var http = require('http');
 var httpProxy = require('http-proxy');
@@ -31,6 +32,8 @@ function writelog(msg) {
 }
 
 function start_app() {
+
+	/*
 	var proxy = httpProxy.createProxyServer({});
 
 	process.chdir("/");
@@ -38,12 +41,15 @@ function start_app() {
 	var server = http.createServer(function (req, res) {
 		try {
 			var domain = tld.registered(''+req.headers.host);
+			writelog("domain =" + util.inspect(domain) );
 			var group = domain.replace(/\./g, '_').toLowerCase();
-			var g = getent.group(group);
+			writelog("group =" + util.inspect(group) );
+			var g = getent.group(group).shift();
+			writelog("g =" + util.inspect(g) );
 			var port = 7000 + g.gid;
-			proxy.web(req, res, { target: 'http://127.0.0.1:' + port }, function() {
-				
-			});
+			writelog("Forwarding to " + port);
+
+			proxy.web(req, res, { target: 'http://127.0.0.1:' + port });
 		} catch(e) {
 			writelog("Error: " + e);
 			if(e.stack) { writelog("" + e.stack); }
@@ -60,6 +66,39 @@ function start_app() {
 			if(e.stack) { writelog("" + e.stack); }
 		}
 	});
+	*/
+
+	var bouncy = require('bouncy');
+	
+	var server = bouncy(function (req, res, bounce) {
+		try {
+			var domain = tld.registered(''+req.headers.host);
+			writelog("domain =" + util.inspect(domain) );
+			var group = domain.replace(/\./g, '_').toLowerCase();
+			writelog("group =" + util.inspect(group) );
+			var g = getent.group(group).shift();
+			writelog("g =" + util.inspect(g) );
+			var port = 7000 + g.gid;
+			writelog("Forwarding to " + port);
+			bounce(port);
+		} catch(e) {
+			writelog("Error: " + e);
+			if(e.stack) { writelog("" + e.stack); }
+		}
+	});
+	server.listen(proxy_port, proxy_host, function() {
+		try {
+			if (process.getuid() === 0) {
+				process.initgroups(proxy_user, proxy_group);
+				process.setgid(proxy_group);
+				process.setuid(proxy_user);
+			}
+		} catch(e) {
+			writelog("Error: " + e);
+			if(e.stack) { writelog("" + e.stack); }
+		}
+	});
+
 }
 	
 try {
