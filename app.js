e37fe96d51037adc6041acc2ab2115d942e61d9c
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-10-16 00:18:02 +0300

    Fixed code

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 /* app.js */
 var argv = require('optimist').argv;
 
+var path = require('path');
 var util = require('util');
 var http = require('http');
 var fs = require("fs");
 var tld = require('tld');
 var getent = require('getent');
+var debug = require('./debug.js');
 
 var _proxy_host = process.env.HOST || '0.0.0.0';
+var _proxy_default_hostname = process.env.HOSTNAME || 'example.com';
 var _proxy_port = process.env.PORT || 80;
-var _proxy_dir = process.env.VARDIR || '/var';
+var _proxy_dir = path.resolve( process.env.VARDIR || '/var' );
 var _proxy_logdir = process.env.LOGDIR || _proxy_dir+'/log';
 var _proxy_rundir = process.env.RUNDIR || _proxy_dir+'/run';
 var _proxy_pid = process.env.PIDFILE || _proxy_rundir + '/nor-web-proxy.pid';
@@ -34,23 +37,51 @@ function writelog(msg) {
 
 /* Get domain from hostname */
 function get_domain(host) {
-	return tld.registered(host);
+	if(typeof host !== 'string') {
+		writelog('Warning! host is not string: ' + util.inspect(host) + " [at "+__filename+":"+ debug.__line +"]" );
+	}
+	host = host.split(':').shift();
+	if(typeof host !== 'string') {
+		writelog('Warning! host is not string: ' + util.inspect(host) + " [at "+__filename+":"+ debug.__line +"]" );
+	}
+	var domain = tld.registered(host);
+	if(typeof domain !== 'string') {
+		domain = host.split('.').filter(function(x){ return x != ''; }).slice(-2).join('.');
+		writelog('Warning! tld detection failed for ' + util.inspect(host) + ', going to use ' + util.inspect(domain) );
+	}
+	return domain;
 }
 
 /* Get group name from domain name */
 function get_group(domain) {
+	if(typeof domain !== 'string') {
+		writelog('Warning! domain is not string: ' + util.inspect(domain)  + " [at "+__filename+":"+ debug.__line +"]" );
+	}
 	return domain.replace(/\./g, '_').toLowerCase();
 }
 
 /* Get system gid by group name */
 function get_gid(group) {
+	if(typeof group !== 'string') {
+		writelog('Warning! group is not string: ' + util.inspect(group) + " [at "+__filename+":"+ debug.__line +"]" );
+	}
 	var g = getent.group(group).shift();
+	if(typeof g.gid !== 'number') {
+		writelog('Warning! g.gid is not number: ' + util.inspect(g.gid) + " [at "+__filename+":"+ debug.__line +"]" );
+	}
 	return g.gid;
 }
 
 /* Get port from request data */
 function get_port(req, res) {
-	var host = ''+req.headers.host;
+
+	var host;
+	
+	if(typeof req.headers.host !== 'undefined') {
+		host = ''+req.headers.host;
+	} else {
+		host = _proxy_default_hostname;
+	}
 
 	if(typeof _cache['byhost_'+host] === 'number') {
 		return _cache['byhost_'+host];
@@ -87,16 +118,14 @@ function drop_privileges() {
 
 /* Start proxy using http-proxy module */
 function start_httpProxy() {
-
 	var httpProxy = require('http-proxy');
 	var proxy = httpProxy.createProxyServer({});
-
-	process.chdir("/");
-
-	var server = http.createServer(function (req, res) {
+	var server = http.createServer(function(req, res) {
 		try {
 			var port = get_port(req, res);
-			proxy.web(req, res, { target: 'http://127.0.0.1:' + port });
+			var url = 'http://127.0.0.1:' + port;
+			writelog('Forwarding ' + util.inspect(req.headers.host) + ' to ' + util.inspect(url) );
+			proxy.web(req, res, { target: url });
 		} catch(e) {
 			writelog("Error: " + e);
 			if(e.stack) { writelog("" + e.stack); }
@@ -111,7 +140,7 @@ function start_bouncy() {
 
 	var bouncy = require('bouncy');
 	
-	var server = bouncy(function (req, res, bounce) {
+	var server = bouncy(function(req, res, bounce) {
 		try {
 			var port = get_port(req, res);
 			bounce(port);
@@ -126,6 +155,8 @@ function start_bouncy() {
 }
 
 try {
+	process.chdir("/");
+
 	if(argv.bouncy) {
 		start_bouncy();
 	} else {
